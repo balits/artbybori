bee3cc27a1c7483998b28bbf29dd873940ed3972
balits <balits.peter.marton@gmail.com>
2023-04-18 18:24:30 +0200

    maybe_fix(instagram aspect square)

--- app/components/homepage/InstagramGallery.tsx
+++ app/components/homepage/InstagramGallery.tsx
@@ -6,16 +6,16 @@ export default function InstagramGallery({mt = 'mt-16'}: {mt?: string}) {
   return (
     <Container
       className={clsx(
-        'mb-10 grid grid-cols-2 grid-rows-4 gap-2 md:grid-cols-4 md:grid-rows-2',
+        'w-full mb-10 grid grid-cols-2 grid-rows-4 gap-2 md:grid-cols-4 md:grid-rows-2',
         mt,
       )}
     >
       {[1, 2, 3, 4, 5, 6, 7, 8].map((v) => (
         <div
           key={v}
-          className="grid place-items-center relative group bg-custom-placeholder-green aspect-square overflow-hidden cursor-pointer transition-all filter hover:brightness-90 active:brightness-70"
+          className="w-full aspect-square grid place-items-center  group bg-custom-placeholder-green cursor-pointer basic-animation filter hover:brightness-90 active:brightness-70"
         >
-          <SiInstagram className="w-1/6 aspect-square text-custom-white opacity-0 transition-opacity  group-hover:opacity-100" />
+          <SiInstagram className="w-8 h-8 lg:w-10 lg:h-10 text-custom-white opacity-0 basic-animation group-hover:opacity-100" />
         </div>
       ))}
     </Container>
