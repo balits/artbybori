a1128c6f86a51e85af78f0ec58af3c76b8a14436
balits <balits.peter.marton@gmail.com>
2023-04-18 18:46:04 +0200

    not finaly

--- app/components/homepage/InstagramGallery.tsx
+++ app/components/homepage/InstagramGallery.tsx
@@ -5,18 +5,22 @@ import {Container} from '../global/Container';
 export default function InstagramGallery({mt = 'mt-16'}: {mt?: string}) {
   return (
     <Container
+      as={"ul"}
       className={clsx(
-        'w-full mb-10 grid grid-cols-2 grid-rows-4 gap-2 md:grid-cols-4 md:grid-rows-2',
+        'mb-10  grid grid-cols-2 grid-rows-4 gap-2 md:grid-cols-4 md:grid-rows-2',
         mt,
       )}
     >
       {[1, 2, 3, 4, 5, 6, 7, 8].map((v) => (
-        <div
+        <li
           key={v}
-          className="w-full aspect-square grid place-items-center  group bg-custom-placeholder-green cursor-pointer basic-animation filter hover:brightness-90 active:brightness-70"
+          className=""
         >
-          <SiInstagram className="w-5 h-5 md:w-8 md:h-8 lg:w-10 lg:h-10 text-custom-white opacity-0 basic-animation group-hover:opacity-100" />
-        </div>
+          <div className='bg-custom-black aspect-square'>
+
+            <SiInstagram className="w-5 h-5 lg:w-8 lg:h-8 aspect-square text-custom-white opacity-0 basic-animation group-hover:opacity-100" />
+          </div>
+        </li>
       ))}
     </Container>
   );
